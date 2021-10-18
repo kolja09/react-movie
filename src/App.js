8bdf9675a1ea68126cf2348f2eec2c80a2801aad
kolja09 <kolja9062005@gmail.com>
2021-10-18 11:08:32 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
